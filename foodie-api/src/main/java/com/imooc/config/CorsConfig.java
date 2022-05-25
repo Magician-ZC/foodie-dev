ec275dd3654902e98c48db74e7ece7183ba3e317
MagicianZC <18850317627@163.com>
2022-05-25 23:56:58 +0800

    1.用户登录成功 2.整合log4j 3.退出清空cookie

--- foodie-api/src/main/java/com/imooc/config/CorsConfig.java
+++ foodie-api/src/main/java/com/imooc/config/CorsConfig.java
@@ -1,5 +1,6 @@
 package com.imooc.config;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
@@ -8,30 +9,35 @@ import org.springframework.web.filter.CorsFilter;
 @Configuration
 public class CorsConfig {
 
-    public CorsConfig(){
-
+    public CorsConfig() {
     }
 
-    public CorsFilter corsFilter(){
-        //1.添加cors配置信息
+    @Bean
+    public CorsFilter corsFilter() {
+        // 1. 添加cors配置信息
         CorsConfiguration config = new CorsConfiguration();
-        config.addAllowedOrigin("http://localhost:8080");
+        config.addAllowedOriginPattern("http://localhost:8080");
+        config.addAllowedOriginPattern("http://shop.z.mukewang.com:8080");
+        config.addAllowedOriginPattern("http://center.z.mukewang.com:8080");
+        config.addAllowedOriginPattern("http://shop.z.mukewang.com");
+        config.addAllowedOriginPattern("http://center.z.mukewang.com");
+        config.addAllowedOriginPattern("*");
 
-        //设置是否发送cookie信息
+        // 设置是否发送cookie信息
         config.setAllowCredentials(true);
 
-        //设置允许请求的方式
+        // 设置允许请求的方式
         config.addAllowedMethod("*");
 
-        //设置允许的header
+        // 设置允许的header
         config.addAllowedHeader("*");
 
-        //2.为url添加映射的路径
+        // 2. 为url添加映射路径
         UrlBasedCorsConfigurationSource corsSource = new UrlBasedCorsConfigurationSource();
-        corsSource.registerCorsConfiguration("/**",config);
+        corsSource.registerCorsConfiguration("/**", config);
 
-        //3.返回重新定义好的corsSource
+        // 3. 返回重新定义好的corsSource
         return new CorsFilter(corsSource);
-
     }
+
 }
