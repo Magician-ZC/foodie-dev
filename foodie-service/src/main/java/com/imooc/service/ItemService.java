dbfc5532bb109596416cbec7955951b21a77f81d
Magician-ZC <zc710932004@gmail.com>
2022-05-29 12:35:31 +0800

    1.查询商品评价等级 2.查询商品评论 3.查询商品详情 4.搜索商品列表 5.获取商品分类 6.获取商品子分类 7.查询每个一级分类下的最新6条商品数据

---
+++ foodie-service/src/main/java/com/imooc/service/ItemService.java
@@ -0,0 +1,78 @@
+package com.imooc.service;
+
+import com.imooc.pojo.*;
+import com.imooc.pojo.vo.CategoryVO;
+import com.imooc.pojo.vo.CommentLevelCountsVO;
+import com.imooc.pojo.vo.ItemCommentVO;
+import com.imooc.pojo.vo.NewItemsVO;
+import com.imooc.utils.PagedGridResult;
+
+import java.util.List;
+
+public interface ItemService {
+
+    /**
+     * 根据商品ID查询详情
+     * @return
+     */
+    public Items queryItemById(String itemId);
+
+    /**
+     * 根据商品ID查询商品图片列表
+     * @param itemId
+     * @return
+     */
+    public List<ItemsImg> queryItemImgList(String itemId);
+
+    /**
+     * 根据商品ID查询商品规格
+     * @param itemId
+     * @return
+     */
+    public List<ItemsSpec> queryItemSpecList(String itemId);
+
+    /**
+     * 根据商品ID查询商品属性
+     * @param itemId
+     * @return
+     */
+    public ItemsParam queryItemParam(String itemId);
+
+    /**
+     * 根据商品id查询商品的评价和等级
+     * @param itemId
+     */
+    public CommentLevelCountsVO queryCommentCounts(String itemId);
+
+    /**
+     * 根据商品id查询商品的评价(分页)
+     * @param itemId
+     * @param level
+     * @return
+     */
+    public PagedGridResult queryPagedComments(String itemId, Integer level,
+                                              Integer page, Integer pageSize);
+
+
+    /**
+     * 搜索商品列表
+     * @param keywords
+     * @param sort
+     * @param page
+     * @param pageSize
+     * @return
+     */
+    public PagedGridResult searchItems(String keywords, String sort,
+                                              Integer page, Integer pageSize);
+
+    /**
+     * 根据分类ID搜索商品列表
+     * @param catId
+     * @param sort
+     * @param page
+     * @param pageSize
+     * @return
+     */
+    public PagedGridResult searchItemsByThirdCat(Integer catId, String sort,
+                                       Integer page, Integer pageSize);
+}
