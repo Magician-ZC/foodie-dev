464f50a6d8123a48fdeafc240b49ed9c3467fb64
MagicianZC <18850317627@163.com>
2022-05-30 21:52:23 +0800

    1.构建支付订单，向商户发送订单

--- foodie-api/src/main/java/com/imooc/controller/OrdersController.java
+++ foodie-api/src/main/java/com/imooc/controller/OrdersController.java
@@ -5,6 +5,8 @@ import com.imooc.enums.PayMethod;
 import com.imooc.pojo.UserAddress;
 import com.imooc.pojo.bo.AddressBO;
 import com.imooc.pojo.bo.SubmitOrderBO;
+import com.imooc.pojo.vo.MerchantOrdersVO;
+import com.imooc.pojo.vo.OrderVO;
 import com.imooc.service.AddressService;
 import com.imooc.service.OrderService;
 import com.imooc.utils.CookieUtils;
@@ -14,8 +16,9 @@ import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
 import org.apache.commons.lang3.StringUtils;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
+import org.springframework.http.*;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.client.RestTemplate;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
@@ -29,6 +32,9 @@ public class OrdersController extends BaseController{
     @Autowired
     private OrderService orderService;
 
+    @Autowired
+    private RestTemplate restTemplate;
+
     @ApiOperation(value = "用户下单",notes = "用户下单",httpMethod = "POST")
     @PostMapping("/create")
     public IMOOCJSONResult create(@RequestBody SubmitOrderBO submitOrderBO, HttpServletRequest request, HttpServletResponse response){
@@ -37,12 +43,31 @@ public class OrdersController extends BaseController{
             return IMOOCJSONResult.errorMsg("支付方式不支持!");
         }
         //1.创建订单
-        String orderId = orderService.createOrder(submitOrderBO);
+        OrderVO orderVO = orderService.createOrder(submitOrderBO);
+        String orderId = orderVO.getOrderId();
         //2.创建订单以后,移除购物车中已结算(已提交)的商品
         //TODO 整合redis之后,完善购物车的已结算商品清除,并且同步到前端的cookie
-        CookieUtils.setCookie(request,response,FOODIE_SHOPCART,"",true);
+       // CookieUtils.setCookie(request,response,FOODIE_SHOPCART,"",true);
 
         //3.向支付中心发送当前订单,用于保存支付中心的订单数据
+        MerchantOrdersVO merchantOrdersVO = orderVO.getMerchantOrdersVO();
+        merchantOrdersVO.setReturnUrl(payReturnUrl);
+
+        //为了方便测试购买，所有的支付金额都改为1分钱
+        merchantOrdersVO.setAmount(1);
+
+        HttpHeaders headers = new HttpHeaders();
+        headers.setContentType(MediaType.APPLICATION_JSON);
+        headers.add("imoocUserId","286440-710932004");
+        headers.add("password","oelp-p2k3-rt0k-39j3");
+
+        HttpEntity<MerchantOrdersVO> entity = new HttpEntity<>(merchantOrdersVO,headers);
+        ResponseEntity<IMOOCJSONResult> responseEntity = restTemplate.postForEntity(paymentUrl,entity,IMOOCJSONResult.class);
+        IMOOCJSONResult paymentResult = responseEntity.getBody();
+        if(paymentResult.getStatus() != 200){
+            return IMOOCJSONResult.errorMsg("支付中心订单创建失败，请联系管理员！");
+        }
+
         return IMOOCJSONResult.ok(orderId);
     }
 
--- foodie-service/src/main/java/com/imooc/service/OrderService.java
+++ foodie-service/src/main/java/com/imooc/service/OrderService.java
@@ -2,6 +2,7 @@ package com.imooc.service;
 
 import com.imooc.pojo.Carousel;
 import com.imooc.pojo.bo.SubmitOrderBO;
+import com.imooc.pojo.vo.OrderVO;
 
 import java.util.List;
 
@@ -11,7 +12,7 @@ public interface OrderService {
      * 创建订单相关信息
      * @return
      */
-    public String createOrder(SubmitOrderBO submitOrderBO);
+    public OrderVO createOrder(SubmitOrderBO submitOrderBO);
 
     /**
      * 修改订单状态
